e7e301e26bccb25b6bbf9d6e223ceab0d54de93b
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-12-23 21:55:37 +0400

    Added a working templates

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask("Inventory Controller")
 
 @app.route("/")
 def hello():
-    return "Hello, world!"
+    return render_template("index.html")
