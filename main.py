139464a99817e627bd5b7a5d6f8050bf6df859d2
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-12-23 22:19:07 +0400

    Ограничил кэш

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask("Inventory Controller")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/")
 def hello():
