d1405a1ec553a36f288e7d1aab690c95be0fe4ae
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-12-24 19:54:34 +0400

    Imported flask_sqlalchemy for working with postgresql

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, request, redirect, current_app
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("Inventory Controller")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/users'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
