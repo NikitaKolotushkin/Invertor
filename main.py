fc5d83e7889749832a583a1965ac211e08f61001
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-12-27 16:57:49 +0400

    Added SECRET_KEY

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, url_for, request, redirect, current_app
+from flask import Flask, render_template, url_for, request, redirect, current_app, session
 from flask_sqlalchemy import SQLAlchemy
 
 import logging
@@ -9,7 +9,7 @@ import logging
 
 app = Flask("Inventory Controller")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/users'
+app.config['SECRET_KEY'] = 'a6c4758dde5b04abbcad31ead65f009c0df2ce16'
 db = SQLAlchemy(app)
 
 @app.route("/")
