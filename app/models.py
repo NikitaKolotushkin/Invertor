969a8ee8ec3a4aa539b04b659b46b715ff945833
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-12-30 17:59:16 +0400

    Added model 'User'

--- app/models.py
+++ app/models.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    full_name = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(32), nullable=True)
+
+    def __repr__(self):
+        return f'Пользователь {self.full_name}'
