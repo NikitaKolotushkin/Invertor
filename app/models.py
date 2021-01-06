0b038eda1f895d03d617b071f4656e718f8215c1
NikitaKolotushkin <nikitaplay1000@gmail.com>
2021-01-06 16:21:46 +0400

    Сделал примерную архитектуру базы данных для пользователя

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from app import db
+import sqlite3
+from app import app, db
 
 
 class User(db.Model):
@@ -10,9 +11,29 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     full_name = db.Column(db.String(255), nullable=False)
     phone = db.Column(db.String(32), nullable=True)
+    password_hash = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
         return f'Пользователь {self.full_name}'
 
 
-# ДОБАВИТЬ КОД СОЗДАНИЯ БД ИЗ РОЛИКА НА ЮТУБЕ!!
+def connect_db():
+    '''
+    Функция для подключения к базе данных
+    '''
+    conn = sqlite3.connect('invertor.db')
+    conn.row_factory = sqlite3.Row
+
+    return conn
+
+
+def create_db():
+    '''
+    Функция для создания таблиц Базы Данных
+    '''
+    db = connect_db()
+    with app.open_resource('../site_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+
+    db.commit()
+    db.close()
